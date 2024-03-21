ba0fac24af44dce79250abdfc0cac83e8946ae1e
alishakhan3 <alisha.khan@accolitedigital.com>
2024-03-21 09:52:33 +0530

    final

--- Counter.java
+++ Counter.java
@@ -6,7 +6,6 @@ public class Counter {
         this.count = 0;
     }
 
-    // Static method to get the singleton instance
     public static Counter getInstance() {
         if (instance == null) {
             instance = new Counter();
@@ -14,12 +13,10 @@ public class Counter {
         return instance;
     }
 
-    // Method to increment the count
     public void increment() {
         count++;
     }
 
-    // Method to get the current count
     public int getCount() {
         return count;
     }
--- singletmethod.java
+++ singletmethod.java
@@ -1,16 +1,15 @@
 public class singletmethod {
     public static void main(String[] args) {
-        Counter counter1 = Counter.getInstance();
-        counter1.increment();
-        counter1.increment();
-        System.out.println("Count from counter1: " + counter1.getCount());
+        Counter obj1 = Counter.getInstance();
+        obj1.increment();
+        obj1.increment();
+        System.out.println("Count from first object= " + obj1.getCount());
 
-        Counter counter2 = Counter.getInstance();
-        counter2.increment();
-        System.out.println("Count from counter2: " + counter2.getCount());
+        Counter obj2 = Counter.getInstance();
+        obj2.increment();
+        System.out.println("Count from second object= " + obj2.getCount());
 
-        // Both counter1 and counter2 point to the same instance
-        System.out.println(counter1 == counter2); // Output: true
+        System.out.println(obj1 == obj2); 
     }
 }
 
